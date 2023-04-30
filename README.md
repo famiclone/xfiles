61dded6648d9f80768ecbfe9aa6b6d90ce94ba46
Denys Tynok <denys@famiclone.dev>
2023-04-30 09:20:13 +0300

    add: subcommands (help)

--- README.md
+++ README.md
@@ -9,9 +9,25 @@ zsh file manager
 ██╔╝ ██╗██║     ██║███████╗███████╗███████║
 ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚══════╝╚══════╝
                                            
+Usage:
+  xfiles
+  xfiles <command>
+
+Commands:
+  xfiles help           Show help
+  xfiles upgrade        Show current and upgrade new version
+  xfiles uninstall      Uninstall
 ```
+
+## Install
+`zsh <(curl -s https://raw.githubusercontent.com/famiclone/xfiles/master/install.zsh)`
 
 ## TODO
 - [x] basic navigation
+- [x] add installer
+- [x] subcommands
+  - [x] help
+  - [ ] update
+  - [ ] uninstall
 - [ ] add, open, delete, rename files
 - [ ] status line
--- install.sh
+++ install.sh
@@ -14,14 +14,14 @@ main() {
     print "No xfilesrc file found, creating..."
     mkdir "$XFILES_CONFIG" &&
     touch "$XFILES_CONFIG"/xfilesrc &&
-    echo "set number" > "$XFILES_CONFIG"/xfilesrc
+    echo "" > "$XFILES_CONFIG"/xfilesrc
     
     print "xfilesrc file created!"
   }
   
   git clone --depth 1 -b $BRANCH https://github.com/famiclone/xfiles "$XFILES_DIR" > /dev/null || { print "Installation was failed!" }
 
-  echo 'Added by xfiles file manager' >> "$ZSHRC"
+  echo '# Added by xfiles file manager' >> "$ZSHRC"
   echo '[ -f "$HOME/.local/share/xfiles/xfiles" ] && alias xfiles="$HOME/.local/share/xfiles/xfiles"' >> "$ZSHRC"
 
   print "xfiles installed!"
